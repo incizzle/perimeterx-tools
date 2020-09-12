ce4bbf2c66b6835fd74ebe6505335389ffa4d95e
Mathew <incizzle@gmail.com>
2020-09-12 04:29:28 -0400

    small changes

--- src/components/Encoder.tsx
+++ src/components/Encoder.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import styled from 'styled-components'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { obfuscateString } from '../services/px';
 
 const Encoder: React.FC = () => {
